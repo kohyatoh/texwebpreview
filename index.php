a2ab5c0ce56b96367093d3ef5305c7d6d8b8018e
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2014-10-25 16:50:14 +0900

    add error display

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-system('./tex/compile.sh');
-header("Content-Type: application/pdf");
-echo file_get_contents("./tex/out.pdf");
-
+system('./tex/compile.sh', $err);
+if ($err == 0) {
+    header("Content-Type: application/pdf");
+    echo file_get_contents("./tex/out.pdf");
+}
+else {
+    echo "<pre>";
+    echo file_get_contents("./tex/log.txt");
+    echo "</pre>";
+}
